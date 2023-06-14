cef1991b7304d8e13aecd443f10e937bb3997cce
AthfanFasee <aththaar47@gmail.com>
2023-06-14 06:08:23 +0530

    Array

--- Array/main.go
+++ Array/main.go
@@ -134,6 +134,25 @@ func threeSum(nums []int) [][]int {
 	return res
 }
 
+func ProductofArrayExceptSelf(nums []int) []int {
+	res := make([]int, len(nums))
+	// [1, 2, 3, 4]
+	prefix := 1
+
+	for i := range nums {
+		res[i] = prefix
+		prefix *= nums[i]
+	}
+
+	postfix := 1
+	for i := len(res) - 1; i >= 0; i-- {
+		res[i] *= postfix
+		postfix *= nums[i]
+	}
+
+	return res
+}
+
 func main() {
 	nums := []int{-1, 0, 1, 2, -1, -4}
 	// fmt.Println(twoSum(nums, 17))
